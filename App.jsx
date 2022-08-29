8110e1ef914bff323460d8e1fdfe03940d681a02
VincentChen6345 <vincent.chen6345@hotmail.com>
2022-08-29 19:16:11 +1000

    useReducer working

--- App.jsx
+++ App.jsx
@@ -1,19 +1,71 @@
 import styles from "./App.module.css";
+import React, { useReducer, useState } from "react";
+
+const ACTIONS = {
+  BLUE: "blue",
+  RED: "red",
+  YELLOW: "yellow",
+  GREEN: "green",
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.BLUE:
+      return {
+        text: "blue",
+        styles: "text-blue",
+      };
+
+    case ACTIONS.RED:
+      return {
+        text: "red",
+        styles: "text-red",
+      };
+
+    case ACTIONS.YELLOW:
+      return {
+        text: "yellow",
+        styles: "text-yellow",
+      };
+
+    case ACTIONS.GREEN:
+      return {
+        text: "green",
+        styles: "text-green",
+      };
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, {
+    text: "blue",
+    styles: "text-blue",
+  });
+  const blue = () => {
+    dispatch({ type: ACTIONS.BLUE });
+  };
+  const red = () => {
+    dispatch({ type: ACTIONS.RED });
+  };
+  const yellow = () => {
+    dispatch({ type: ACTIONS.YELLOW });
+  };
+  const green = () => {
+    dispatch({ type: ACTIONS.GREEN });
+  };
   return (
     <div className={styles.App}>
       <main className={styles.container}>
         <span>
-          <button>Blue</button>
-          <button>Red</button>
-          <button>Yellow</button>
-          <button>Green</button>
-          <div>blue</div>
+          <button onClick={blue}>Blue</button>
+          <button onClick={red}>Red</button>
+          <button onClick={yellow}>Yellow</button>
+          <button onClick={green}>Green</button>
+          <div className={styles[state.styles]}>{state.text}</div>
         </span>
       </main>
     </div>
   );
 }
-
 export default App;
